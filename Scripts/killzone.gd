32293d15ceae5f5b7aa41aec53975eca27e34c2d
Funnysloth12 <b_rison12@outlook.com>
2026-01-09 15:08:56 -0500

    Lots of features!
    added the ability pickup coins, and a partially finished respawn system

---
+++ Scripts/killzone.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+func _ready():
+	print("Area2D ready, monitoring:", monitoring, "collision shape:", $CollisionShape2D.disabled)
+
+
+@onready var timer: Timer = $Timer
+
+func _on_body_entered(_body):
+	print("U suck at the game")
+	timer.start()
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
