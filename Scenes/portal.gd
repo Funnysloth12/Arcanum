d5184c81da3c3caf740afbb8d2fd794cdf0c20b6
Funnysloth12 <b_rison12@outlook.com>
2026-01-22 18:03:21 -0500

    nothing

--- Scenes/portal.gd
+++ Scenes/portal.gd
@@ -1,15 +1,5 @@
 extends Area2D
 
-var entered = false
-
-func _on_body_entered(_body: PhysicsBody2D) -> void:
-	entered = true
-
-func _on_body_exited(_body: Node2D) -> void:
-	entered = false
-	
-	
 func _process(_delta):
-	if entered == true:
-		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene("res://Levels/level2.tscn")
+	for body in get_overlapping_bodies():
+		print("OVERLAP:", body.name)
